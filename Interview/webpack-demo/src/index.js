c7fddec0e31da9dad1c5dd10e09a243fe0514743
qiyulei <qiyulei@gridsum.com>
2020-08-24 18:03:21 +0800

    08-24webpack loader

--- Interview/webpack-demo/src/index.js
+++ Interview/webpack-demo/src/index.js
@@ -1,2 +1,11 @@
 console.log('webpack');
-import './index.css'
+import pic  from './images/dingkun.gif';
+
+import './css/index.css'
+import './css/index.less'
+
+var img = new Image()
+img.src = pic;
+img.classList.add('logo')
+var root = document.getElementById('root')
+root.append(img)
--- Interview/webpack-demo/webpack.config.js
+++ Interview/webpack-demo/webpack.config.js
@@ -1,6 +1,6 @@
 // webpack，基于nodejs，遵守commonjs规范
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const htmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 module.exports = {
   mode: 'development',
@@ -26,7 +26,12 @@ module.exports = {
   },
   // 插件
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new htmlWebpackPlugin({
+      title: 'lei',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
   ],
   // 处理不认识的模块
   module: {
@@ -37,6 +42,41 @@ module.exports = {
         // css-loader是把css模块的内容加到js模块中，即css in js方式
         // style-loader 从js中提取css的loader, 在html中创建style标签，把css内容放在这个style标签中
         use:  ['style-loader','css-loader']
+      },
+      {
+        test: /\.less$/,
+        // loader的执行顺序从后往前
+        // css-loader是把css模块的内容加到js模块中，即css in js方式
+        // style-loader 从js中提取css的loader, 在html中创建style标签，把css内容放在这个style标签中
+        // use: ["style-loader", "css-loader", "less-loader"]
+        use: ['style-loader',  {
+          loader: 'css-loader',
+          options: {
+            // 开启css 模块化
+            modules: true
+          }
+        }, {
+          loader: 'postcss-loader',
+        }, "less-loader"]
+      },
+      {
+       test: /\.(png|jpe?g|gif)$/,
+       use: {
+        //  loader: 'file-loader',
+        // 推荐使用url-loader,因为支持limit
+         loader: 'url-loader',
+         options: {
+          //  ext占位符
+           name: '[name]_[hash:6].[ext]',
+           outputPath: 'images/',
+           //  小于2048，才转换成base64
+           limit: 2048
+         }
+       }
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        use: 'url-loader'
       }
     ]
   }
